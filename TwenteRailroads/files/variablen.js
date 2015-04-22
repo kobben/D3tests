581d378bc9a60606762aa00960244503b07b7bc7
kobben <kvastmo64>
2015-04-22 17:21:44 +0200

    updated and extended trackViewer examples...

---
+++ TwenteRailroads/files/variablen.js
@@ -0,0 +1,34 @@
+// Author: ITC/Creator: MapViewSVG 2.2 by uismedia (http://www.mapview.de); 25/03/03 19:47:59
+var SVGmap;
+var svgSVGmap;
+var mapOrigX;
+var mapOrigY;
+var mapOrigWidth;
+var mapOrigHeight;
+var mapBrowseWidth;
+var mapBrowseHeight;
+var theBGColor = 'white';
+var theHiColor = 'yellow';
+var closeButText = "Close Window";
+var QueryNoRecText = "No Records found...";
+var SVGbottom; 
+var SVGlegend;
+var oldRec = '';
+var oldColor = '';
+var oldProp = '';
+var BrowserSet = 'width=400,height=400,top=50,left=50,toolbar=no,menubar=no,location=no,hotkeys=no,resizable=yes,scrollbars=no,dependent=yes,status=no';
+var ObjectHeadText = "Object Information";
+var ObjectLinkText = "Further information";
+var zoomScrollBoolean;
+var massZahl;
+var overRectBoolean;
+var SVGSupportText = "This website uses the free AdobeSVG Viewer 3.0 Plug-In.\n(http://www.adobe.com/svg/viewer/install/main.html)";
+var theOverviewFlag = false;
+var theLegendFlag = true;
+var theGetscaleFlag = false;
+var theCheckThemeScaleFlag = true;
+var theScaleConvert = 100;
+var theScalebarFlag = false;
+var CheckStatus = 2400;
+var theFontSize = 15;
+
